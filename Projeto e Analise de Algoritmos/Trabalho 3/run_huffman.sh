a78dcd8fe4e4e6fe6fe6d112825339fdd535a437
meitcher <marcosvtreviso@gmail.com>
2014-02-26 21:58:56 -0300

    Update compress method

--- Projeto e Analise de Algoritmos/Trabalho 3/run_huffman.sh
+++ Projeto e Analise de Algoritmos/Trabalho 3/run_huffman.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
 cd python/
-printf "um bom corredor nao precisa nunca ter medo do escuro" > entrada
+file="entrada"
+
+if [ ! -f "$file" ]; then
+	printf "um bom corredor nao precisa nunca ter medo do escuro" > $file
+fi
+
 python3 main.py
