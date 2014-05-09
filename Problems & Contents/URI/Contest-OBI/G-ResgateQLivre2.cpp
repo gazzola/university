277722eb86f79de9a1b0b29e3b4d8e28ace5b079
meitcher <marcosvtreviso@gmail.com>
2014-05-09 18:05:05 -0300

    Add solutions

--- Problems & Contents/URI/Contest-OBI/G-ResgateQLivre2.cpp
+++ Problems & Contents/URI/Contest-OBI/G-ResgateQLivre2.cpp
@@ -1,12 +1,16 @@
 #include <iostream>
 #include <cstdio>
+#include <cstring>
 #include <vector>
 #include <cmath>
+#include <algorithm>
 
 using namespace std;
 
 
-const double INF = 9999999;
+#define INF 9999999;
+
+
 typedef pair<int, int> ii;
 double matAdj[10001][10001];
 
@@ -17,8 +21,29 @@ double distancia(ii p1, ii p2){
 	return sqrt((x*x) + (y*y));
 }
 
+template <class T>
+void printVector(const vector<T> &v){
+	for(int i=0; i<v.size(); i++)
+		printf("(%d, %d)  ", v[i].first, v[i].second);
+ 	printf("\n");
+}
 
-//can be bsearch O(lg n)
+
+void showVector(vector<int> &v){
+	for(int i=0; i<v.size(); i++)
+		printf("%d ", v[i]);
+ 	printf("\n");
+}
+
+
+template <class T>
+bool orderBy(const pair<T, T> &i, const pair<T, T> &j){
+	ii temp = ii(0, 0);
+	return (distancia(temp, i) < distancia(temp, j));
+}
+
+
+//bsearch
 int isSet(vector<int> &vertices, int v){
 	for(int i=0; i<vertices.size(); i++)
 		if(vertices[i] == v)
@@ -73,7 +98,8 @@ void *prim(vector<int> &vertices, int r, double *chave, int *pi){
 
 int main(){
 
-	int c, n, x, y;
+	int c, n;
+	double x, y;
 
 	scanf("%d", &c);
 	while(c--){
@@ -83,7 +109,7 @@ int main(){
 
 		scanf("%d", &n);
 		for(int i=0; i<n; i++){
-			scanf("%d %d", &x, &y);
+			scanf("%lf %lf", &x, &y);
 			pessoas.push_back(ii(x, y));
 			vertices.push_back(i);
 
@@ -95,14 +121,59 @@ int main(){
 		}
 
 
-		double chave[10001], soma=0;
+		double soma = 0;
+		double chave[10001];
 		int pi[10001];
 
 		prim(vertices, 0, chave, pi);
 		for(int i=1; i<n; i++)
 			soma += matAdj[i][pi[i]];
 
+		printf("%.2lf\n", soma/100); //em metros
+
+
+		/*
+		printVector(pessoas);
+		sort(pessoas.begin(), pessoas.end(), orderBy<int>);
+		printVector(pessoas);
+
+		int tam = pessoas.size();
+		double soma = 0;
+		ii last = pessoas[0];
+ 
+		for(int i=1; i<tam; i++){
+ 			soma += distancia(last, pessoas[i]);
+			last = pessoas[i];
+		}
+
 		printf("%.2lf\n", soma/100.0); //em metros
+		*/
+
+
+		/*int i=0;
+		double menor, soma=0;
+
+		while(pessoas.size() > 0){
+
+			ii atual = pessoas[i]; 
+			pessoas.erase(pessoas.begin()+i); //remove(v[i]);
+			menor = INF;
+
+			for(int j=0; j<pessoas.size(); j++){
+				double dist = distancia(atual, pessoas[j]);
+				if(dist < menor){
+					menor = dist;
+					i = j;
+				}
+			}
+			
+			soma += distancia(atual, pessoas[i]);
+
+		}
+
+		printf("%.2lf\n", soma/100.0); //em metros
+		*/
+
 
 	}
 
