52a3144e15c561d0d4042f5628a74882f2c89925
meitcher <marcosvtreviso@gmail.com>
2014-09-18 15:36:10 -0300

    Add solutions

--- Problems & Contents/URI/2-Ad-Hoc/1591-DiaDaVovo.cpp
+++ Problems & Contents/URI/2-Ad-Hoc/1591-DiaDaVovo.cpp
@@ -7,7 +7,7 @@
 using namespace std;
 
 
-void printMatrix(char matrix[110][110], int n){
+void printMatrix(char matrix[50][51], int n){
 	for(int i=0; i<n; i++)
 		printf("%s\n", matrix[i]);
 	printf("\n");
@@ -17,7 +17,7 @@ void printMatrix(char matrix[110][110], int n){
 int countOccurrences(char *from, char *sub){
 	
 	int total = 0;
-	char* tmp = from;
+	char *tmp = from;
 
 	while((tmp = strstr(tmp, sub)) != NULL){
 		total++;
@@ -28,14 +28,47 @@ int countOccurrences(char *from, char *sub){
 }
 
 
+int contar(char *from, char *sub){
+
+	int tamf = strlen(from);
+	int tams = strlen(sub);
+
+	if(tams > tamf)
+		return 0;
+
+	if(tams == tamf){
+		if(strcmp(from, sub) == 0)
+			return 1;
+		return 0;
+	}
+
+	int count = 0;
+	bool ok;
+	for(int i=0; i<=tamf-tams; i++){
+
+		ok = true;
+		for(int j=0; j<tams; j++){
+			if(from[i+j] != sub[j]){
+				ok = false;
+				break;
+			}
+		}
+		if(ok)
+			count++;
+	}
+
+	return count;
+}
+
 
 int main(){
 	
 	
 	int t, l, c, p;
-	char linhas[110][110];	// linhas normais
-	char colunas[110][110];	// colunas invertidas 
-	char palavra[110];
+	char linhas[50][51];	// linhas normais
+	char colunas[50][51];	// colunas invertidas 
+	char palavra[51];
+
 
 	scanf("%d", &t);
 	while(t--){
@@ -43,13 +76,14 @@ int main(){
 		scanf("%d %d", &l, &c);
 
 		for(int i=0; i<l; i++){
-			scanf("%s", palavra);
-			strcpy(linhas[i], palavra);
 
-			for(int j=0; j<c; j++)
+			scanf("%s", linhas[i]);
+			linhas[i][c] = '\0';
+
+			for(int j=0; j<c; j++){
 				colunas[j][i] = linhas[i][j];
-
-			colunas[i][l] = '\0';
+				colunas[j][l] = '\0';
+			}
 		}
 
 
@@ -58,11 +92,13 @@ int main(){
 					
 			scanf("%s", palavra);
 
-			int total = 0;	
-			for(int i=0; i<l; i++)
-				total += countOccurrences(linhas[i], palavra);
+			int total = 0;
+			int tam = strlen(palavra);
+			if(tam <= l)
+				for(int i=0; i<l; i++)
+					total += countOccurrences(linhas[i], palavra);
 
-			if(strlen(palavra) > 1)
+			if(tam > 1 and tam <= c)
 				for(int i=0; i<c; i++)
 					total += countOccurrences(colunas[i], palavra);
 
