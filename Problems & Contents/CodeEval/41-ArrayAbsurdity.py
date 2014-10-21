597aa0fd7ddbab341f684ebdd05ee3d9045e669a
meitcher <marcosvtreviso@gmail.com>
2014-10-21 01:05:30 -0200

    Add CodeEval solutions

--- Problems & Contents/CodeEval/41-ArrayAbsurdity.py
+++ Problems & Contents/CodeEval/41-ArrayAbsurdity.py
@@ -8,16 +8,26 @@ import sys
 def total(n):
     return (n*n - 3*n + 2)/2
 
+
+def method1(cases, n):
+    t = total(n)
+    s = 0
+    for x in cases.split(","):
+        s += int(x)
+    return int(s-t)
+
+def method2(cases, n):
+    a = 0
+    for i, x in enumerate(cases.split(",")):
+        a = a ^ (int(x)+1) ^ i
+    return (a-1)
+
 test_cases = open(sys.argv[1], 'r')
 for test in test_cases:
 
     cases = test.split(";")
     n = int(cases[0])
-    t = total(n)
-    s = 0
-    for x in cases[1].split(","):
-        #print(x)
-        s += int(x)
-    print(int(s-t))
+    print(method2(cases[1], n))
+    
 
 test_cases.close()
