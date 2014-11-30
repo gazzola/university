445462256757a60a400824bad0ec79295f77c370
meitcher <marcosvtreviso@gmail.com>
2014-11-30 15:48:58 -0200

    Add trabs calc num

--- Calculo Numerico/Trabalho 14/Trabalho14.py
+++ Calculo Numerico/Trabalho 14/Trabalho14.py
@@ -1,19 +1,30 @@
 import math
 
 
+def print_res(res):
+	for x, y in res:
+		print("y(%.2f) = %.4f" % (x, y))
+
+
+def eq(x, y):
+	return x - y + 2
 
 def next_y(x, y, h):
-	return h*(1 - h/x)*y
+	return y + h*eq(x, y)
 
 
-def next_x(x, h):
-	return x+h
-
-
-
-def euler(x0, h):
+def euler(x0, xn, y0, h):
 
 	y = []
-	y.append(x0)
+	y.append((x0, y0))
 
-	while
+	x = x0+h
+	while x <= xn+h:
+		y.append((x, next_y(x, y[-1][1], h)))
+		x += h 
+
+	return y
+
+
+result = euler(0, 1, 2, 0.01)
+print_res(result)
